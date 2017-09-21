6e5f31f7a9ce50294af6105a0b9e2a430cf8f89a
ytngl <taniayap@gmail.com>
2017-09-21 10:58:17 -0700

    Refactor code, add comments

--- weather_fetcher.py
+++ weather_fetcher.py
@@ -10,25 +10,24 @@ from sys import argv
 # Users key goes here
 key = ""
 
+# Zipcode caught from user input
 zipcode = argv[1]
 
 # look-up location
-# IP API
 ip = os.popen('curl -s ifconfig.io').read()
 ip_location = requests.get('http://ip-api.com/json')
 ip_data = json.loads(ip_location.content)
-# zip_code=ip_data["zip"]
 
-zip_code = zipcode
 
 # weather API
-parameter = {'key': key, 'q': zip_code}
+parameter = {'key': key, 'q': zipcode}
 weather = requests.get("http://api.apixu.com/v1/current.json", params=parameter)
 
 # parsing weather data
 w_data = json.loads(weather.content)
 
-print("Location: " + zip_code + " --" + w_data["location"]["name"] + ", " + w_data["location"]["region"])
+# Printing data
+print("Location: " + zipcode + " --" + w_data["location"]["name"] + ", " + w_data["location"]["region"])
 print("Temperature: " + str(w_data["current"]["temp_c"]) + "C" + "/" + str(w_data["current"]["temp_f"]) + "F")
 print("Wind Speed: " + str(w_data["current"]["wind_mph"]) + " mph")
 print("Precipitation: " + str(w_data["current"]["wind_mph"]) + " in")
